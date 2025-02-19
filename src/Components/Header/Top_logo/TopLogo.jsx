f3692c5dd4d3d9985cb2d33fef6f834e7b1e87ce
Marawan Saqr <loew.krs@yahoo.com>
2025-02-19 22:57:04 +0200

    Added Dark Mode

--- src/Components/Header/Top_logo/TopLogo.jsx
+++ src/Components/Header/Top_logo/TopLogo.jsx
@@ -1,9 +1,26 @@
 import './TopLogo.css';
-import React from 'react';
+import React, { useEffect } from 'react';
 import {Container, Navbar, Nav} from 'react-bootstrap';
+import { useState } from 'react';
 
 
 const TopLogo = () => {
+
+  // Component States
+  const [theme, setTheme] = useState(localStorage.getItem("theme") || "lightMode");
+
+  const changeTheme = () => {
+    const newTheme = theme === "lightMode" ? "darkMode" : "lightMode";
+    document.body.classList.toggle("darkMode");
+    setTheme(newTheme);
+    localStorage.setItem("theme", newTheme);
+  };
+
+  useEffect(() => {
+    document.body.classList.add(theme);
+  }, [theme]);
+
+
   return (
     <div className='top-logo'>
       <Navbar className="bg-body-tertiary">
@@ -19,7 +36,7 @@ const TopLogo = () => {
         </Navbar.Collapse>
         <Navbar.Toggle />
         <Navbar.Collapse className="justify-content-end">
-          <Navbar.Brand><a><i className="fas fa-search"></i></a></Navbar.Brand>
+          <Navbar.Brand><a onClick={changeTheme} style={{cursor: 'pointer'}}>{theme === "darkMode" ? (<i className='fas fa-sun'></i>) : (<i className="fas fa-moon"></i>)}</a></Navbar.Brand>
         </Navbar.Collapse>
       </Container>
     </Navbar>
