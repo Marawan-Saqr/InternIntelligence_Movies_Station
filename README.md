de56f295b722b313dc2626cbadcfbd7938a49cd8
Marawan Saqr <loew.krs@yahoo.com>
2025-02-27 00:01:31 +0200

    End Watchlist Component

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-1-Watchlist.
-2-Edit Optional.
-3-Footer.
-4-Loading In Each API.
-5-Animations.
+1-Edit Optional.
+2-Footer.
+3-Loading In Each API.
+4-Animations.
--- src/Components/Watchlist/Watchlist.jsx
+++ src/Components/Watchlist/Watchlist.jsx
@@ -1,22 +1,39 @@
-import React from 'react';
-import './Watchlist.css';
-import { Container } from 'react-bootstrap';
-import { watchlistContext } from '../../Contexts/WatchList.jsx';
-import { useContext } from 'react';
+import React from "react";
+import "./Watchlist.css";
+import { Container, Row, Col } from "react-bootstrap";
+import { watchlistContext } from "../../Contexts/WatchList.jsx";
+import { useContext } from "react";
+import WatchlistCard from "./Watchlist_card/WatchlistCard.jsx";
 
 const Watchlist = () => {
-
   // Component States
   const { watch } = useContext(watchlistContext);
 
-
   return (
-    <div>
+    <div className="watchlist mt-5 mb-5">
       <Container>
-        
+        <Row>
+          {watch.length >= 1 ? (
+            watch.map((element) => (
+              <>
+                <h4>Watclist</h4>
+                <Col xs={4} sm={4} md={6} lg={3} key={element.id}>
+                  <WatchlistCard element={element} />
+                </Col>
+              </>
+            ))
+          ) : (
+            <h2
+              className="text-center"
+              style={{ textTransform: "uppercase", fontWeight: "bold" }}
+            >
+              No Movies At Watchlist
+            </h2>
+          )}
+        </Row>
       </Container>
     </div>
-  )
-}
+  );
+};
 
 export default Watchlist;
