b03ee516e01e0f4846626f0049fe3b1cc08d7569
Marawan Saqr <loew.krs@yahoo.com>
2025-02-27 19:21:03 +0200

    Added Not Found Page

--- src/Pages/Home/Home_cover/HomeCover.jsx
+++ src/Pages/Home/Home_cover/HomeCover.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+import Loader from '../../../Components/Loading/Loading.jsx';
+import { movieContext } from '../../../Contexts/MovieContext.jsx';
+import { useContext } from 'react';
 import "./HomeCover.css";
 
 const HomeCover = () => {
+
+  // Component States
+  const { loading } = useContext(movieContext);
+
+
   return (
     <div className="home-cover">
+      {loading && <Loader />}
       <Container>
         <div className="content">
           <h2>
